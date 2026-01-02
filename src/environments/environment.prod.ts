2168d621e745d7c9c2678503fb10972d86607e85
adithramesh <adithramesh90@gmail.com>
2026-01-02 22:28:16 +0530

    add environment production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  BACK_END_API_URL: 'https://mean-blog-app-d8nz.onrender.com',
+  BACK_END_API_URL: 'https://cms-backend-nestjs-gaxl.onrender.com',
   CLOUDINARY_CLOUD_NAME: 'dj8j4pwqk'
 };
